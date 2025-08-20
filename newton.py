001776e84bf1aa12d89aff3d63b70262bd2567c2
Xinpeng Chen <xinpeng.chen@berkeley.edu>
2025-08-20 10:27:38 -0700

    Add basic implementation of Newton's method.

---
+++ newton.py
@@ -0,0 +1,10 @@
+def newton(x0, f):
+    eps = 0.000001
+    diff = 1
+    while abs(diff) > eps:
+        f1 = (f(x0 + eps) - f(x0 - eps)) / (eps*2)
+        f2 = (f(x0 + eps) - 2 * f(x0) + f(x0 - eps)) / (eps**2)
+        diff = f1/f2
+        x0 = x0 - diff
+        print(x0, diff)
+    return x0
