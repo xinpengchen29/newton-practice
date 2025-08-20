42b9a6b50bec4988e43d84df8144d3cee1357d1f
Xinpeng Chen <xinpeng.chen@berkeley.edu>
2025-08-20 13:57:30 -0700

    Add ruff checked and formatted versions of newton.

--- newton-save.py
+++ newton-save.py
@@ -3,9 +3,9 @@ def newton(x0, f):
     eps = 1e-5
     diff = 1
     while abs(diff) > eps:
-        f1 = (f(x0 + eps) - f(x0 - eps)) / (eps * 2)
+        f1 = (f(x0 + eps) - f(x0 - eps)) / (eps*2)
         f2 = (f(x0 + eps) - 2 * f(x0) + f(x0 - eps)) / (eps**2)
-        diff = f1 / f2
+        diff = f1/f2
         x0 = x0 - diff
         print(x0, diff)
     return x0
