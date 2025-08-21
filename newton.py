30033aea412e83e91494597ef950bb47e6b7960e
Xinpeng Chen <xinpeng.chen@berkeley.edu>
2025-08-21 10:18:26 -0700

    Add multivariate Newton method.

--- newton.py
+++ newton.py
@@ -1,8 +1,10 @@
-def newton(x0, f):
+import numpy as np
+from scipy.differentiate import jacobian
+
+def newton(x0, f, eps=1e-6):
     """Returns the numerical approximation of a function using Newton Raphson."""
     if not callable(f): #check for valid arguments
         raise TypeError(f"Argument is not a function, it is of type {type(f)}")
-    eps = 1e-5
     diff = 1
     iter = 0
     while abs(diff) > eps:
@@ -17,3 +19,27 @@ def newton(x0, f):
         if x0 > 1e7: #check for divergence
             raise RuntimeError(f"At iteration {iter}, optimization appears to be diverging")
     return x0
+
+def multi_newton(x0, f, eps=1e-6):
+    """Returns the numerical approximation of a multivariate function using Newton Raphson."""
+    if not callable(f): #check for valid arguments
+        raise TypeError(f"Argument is not a function, it is of type {type(f)}")
+    diff = 1
+    max_iter = 200
+    for i in range(max_iter):
+        # Check for convergence
+        if diff < eps:
+            print(f"Converged in {i+1} iterations.")
+            return x0
+        try:
+            # Solve the linear system J(x) * delta_x = -F(x) for delta_x
+            delta_x = np.linalg.solve(jacobian(f, x0), -f(x0))
+            x0 = x0 + delta_x
+        except np.linalg.LinAlgError:
+            print("Jacobian matrix is singular, cannot proceed.")
+            return None
+        iter += 1
+        diff = np.linalg.norm(delta_x)
+        print(x0, diff)
+    print("Maximum iterations reached, no convergence.")
+    return None
